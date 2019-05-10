283f013b63ad0b3ee7a7d49daee86830019fd31f
Kobchai Rattanacomepee <kobchai@gmail.com>
2019-05-10 14:37:02 +0700

    commit chapter 7

--- server/src/app.js
+++ server/src/app.js
@@ -2,21 +2,29 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
+
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routers')(app)
 
-// app.get('/status', function(req, res) {
-//   res.send('Hello nodejs server')
-// })
+app.get('/status', function(req, res) {
+  res.send('Hello nodejs server')
+})
 
-// app.get('/hello/:person', function(req, res){
-//   console.log('Hello - ' + req.params.person)
-//   res.send('say hello with ' + req.params.person)
-// })
+app.get('/hello/:person', function(req, res){
+  console.log('Hello - ' + req.params.person)
+  res.send('say hello with ' + req.params.person)
+})
+
+app.post('/hello', function (req, res) {
+  res.send('OK you post - ' + req.body.name)
+})
 
 // // get user by id
 // app.get('/user/:userId', function(req, res){
@@ -45,7 +53,14 @@ require('./routers')(app)
 //   JSON.stringify(req.body))
 // })
 
-let port = 8081
-app.listen(port, function(){
-  console.log('server running on port ' + port)
+// let port = 8081
+// app.listen(port, function(){
+//   console.log('server running on port ' + port)
+// })
+
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+  app.listen(port, function () {
+    console.log('Server running on ' + port)
+  })
 })
